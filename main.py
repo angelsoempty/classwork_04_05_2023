b8c979290ec1b6b826f7fdc7f2cf6887dd1e7041
angelsoempty <denatamanov1@gmail.com>
2023-05-04 20:27:40 +0300

    #2 завдання

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def info(self):
-        print(f"Ім'я: {self.name}, Вік: {self.age}")
-student = Student('Vasya', 23)
-student.info()
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return  3.14 * self.radius ** 2
+circle = Circle(5)
+print(circle.area())
