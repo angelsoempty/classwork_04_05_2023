2bb455c78b443c8c7d3ecaf6be90d7ff2403cf44
angelsoempty <denatamanov1@gmail.com>
2023-05-04 20:45:15 +0300

    #4 завдання

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-class Animals:
-    pass
-class Dog(Animals):
-    def sound(self):
-        print('ГАААВ!')
-pes_patron = Dog()
-pes_patron.sound()
+class BankAccount:
+    def __init__(self, owner, balance = 0):
+        self.owner = owner
+        self.balance = balance
+    def deposit(self, amount):
+        self.balance += amount
+    def withdraw(self, amount):
+        if self.balance - amount < 0:
+            print('Недостатньо коштів')
+        else:
+            self.balance -= amount
+    def check_balance(self):
+        return self.balance
+acc = BankAccount('Vasya', 500)
+acc.deposit(200)
+acc.withdraw(100)
+print(acc.check_balance())
